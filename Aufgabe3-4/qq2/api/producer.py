d2a4fbe49e286e2dae5993a3fae0430e961a84c5
Daniel Holbach <holbach@d-sire.com>
2018-10-18 13:49:24 +0200

    Aufgabe 4

---
+++ Aufgabe3-4/qq2/api/producer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(bootstrap_servers=['qq2.ddnss.de:9092'])
+topic = 'logging'
+serviceName = '1_Django_1'
+
+def log(operation, message):
+    loggingMessage = { "service_name": serviceName, "operation": operation, "message": message }
+    producer.send(topic, json.dumps(loggingMessage))
--- Aufgabe3-4/qq2/api/views.py
+++ Aufgabe3-4/qq2/api/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Student
+from.producer import log
 
 # Create your views here.
 def index(request):
@@ -11,16 +12,19 @@ def allStudents(request):
     if request.method == 'GET':
         students = Student.objects.all().values()
         students_list = list(students)
+        log(request.method, 'all students')
         return JsonResponse(students_list, safe=False)
     elif request.method == 'POST':
         json_data = json.loads(request.body)
         newStudent = Student(firstname=json_data['firstname'], lastname=json_data['lastname'], matriculation_number=json_data['matriculation_number'], course=json_data['course'], email=json_data['email'])
         newStudent.save()
+        log(request.method, 'New student with matriculation number ' + str(json_data['matriculation_number']))
         return JsonResponse(json_data)
 
 def oneStudent(request, mnr):
     student = Student.objects.get(matriculation_number=mnr)
     if request.method == 'GET':
+        log(request.method, 'student with matriculation number ' + str(mnr))
         return JsonResponse(student.to_dict(), safe=False)
     elif request.method == 'PATCH':
         json_data = json.loads(request.body)
@@ -30,7 +34,9 @@ def oneStudent(request, mnr):
         student.course = json_data['course']
         student.email = json_data['email']
         student.save()
+        log(request.method, 'student with matriculation number ' + str(mnr))
         return JsonResponse(student.to_dict(), safe=False)
     elif request.method == 'DELETE':
         student.delete()
+        log(request.method, 'student with matriculation number ' + str(mnr))
         return HttpResponse()
